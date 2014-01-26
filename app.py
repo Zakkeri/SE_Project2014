1a1c5ace881867b47523250c5556933531fc16b9
Andrew Calvano <acalvano@Erebus.(none)>
2014-01-26 09:07:28 -0800

    Added a basic flask applications barebones

---
+++ app.py
@@ -0,0 +1,18 @@
+import sys
+import os
+from flask import Flask, request, session, url_for, \
+    abort, render_template, flash, redirect, Response
+
+app = Flask(__name__)
+app.debug = True
+app.config.from_object(__name__)
+app.config.from_envvar('FLASKR_SETTINGS', silent = True)
+
+app.secret_key = os.urandom(24)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run()
