4872d5f6c8a8ab76cc2a0100f89fee4f86d718ba
Andrew Calvano <acalvano@Erebus.(none)>
2014-01-26 12:54:12 -0800

    Working login with admin:test credentials, far from complete, templates need to be heavily modified to adjust for user session verse guest session

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import sys
 import os
 from flask import Flask, request, session, url_for, \
     abort, render_template, flash, redirect, Response
+from dbmodels import * #Importing db interface from sqlalchemy
 
 app = Flask(__name__)
 app.debug = True
@@ -10,12 +11,53 @@ app.config.from_envvar('FLASKR_SETTINGS', silent = True)
 
 app.secret_key = os.urandom(24)
 
+#Setup DB here
+db.create_all()
+
+#For creating the inital admin user who will be able
+#to create/modify/delete other users inside of the application
+if User.query.filter_by(uname="admin").first():
+    pass
+else:
+    admin = User("admin", "test", 1)
+    db.session.add(admin)
+    db.session.commit()
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+
+    if 'username' in session:
+        abort(401)
+
+    success = 0 # Represents whether the login was sucessful
+                # Effects the templating system based on its value
+
+    if request.method == "POST": # Check for HTTP POST Request
+
+        checkuser = request.form['username']
+        password = request.form['password']
+
+        user_exists = User.query.filter_by(uname=checkuser).first()
+
+        if user_exists:
+
+            #This logic will need to be changed for security reasons
+            #We don't want to store passwords in plaintext in the database
+            if password == user_exists.password:
+
+                success = 1 # Value is set to 1 if the correct password is entered
+                session['username'] = user_exists.uname
+
+                if user_exists.isadmin:
+                    session['isadmin'] = True
+                else:
+                    session['isadmin'] = False
+                return redirect(url_for("home"))
+
     return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
--- dbmodels/__init__.py
+++ dbmodels/__init__.py
@@ -5,22 +5,22 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:test@127.0.0.1/cardb'
 db = SQLAlchemy(app)
 
-class Users(db.Model):
+class User(db.Model):
     """Class to represent the Users table.  This table
        represents acutal users of the application
        
        UID | Username | Password | IsAdmin
     """
 
-    uid = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.Integer, primary_key=True, autoincrement=True)
     uname = db.Column(db.String(45), unique=True)
     password = db.Column(db.String(45), unique=True)
     isadmin = db.Column(db.Boolean)
 
-    def __init__(self, uid, uname, password):
-        self.uid = uid
+    def __init__(self, uname, password, isadmin):
         self.uname = uname
         self.password = password
+        self.isadmin = isadmin
 
     def __repr__(self):
         return '<User %r>' % self.uname
